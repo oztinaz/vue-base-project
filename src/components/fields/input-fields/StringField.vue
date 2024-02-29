0d822331a70ddca2fb248c42b48084f8fa5cfdfc
Oğuz Öztınaz <oguzoztinaz0@gmail.com>
2024-02-29 17:12:57 +0300

    Implement StringField component

---
+++ src/components/fields/input-fields/StringField.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import VInputField from './VInputField.vue'
+import { ref, type Ref } from 'vue'
+import type { Validation } from '@/types/Validation'
+
+const string = defineModel('string', {
+  required: true,
+  type: String
+})
+
+const props = defineProps<{
+  placeholder?: string
+  validations?: Validation[]
+}>()
+
+const inputClass: Ref<string> = ref('')
+</script>
+
+<template>
+  <VInputField :input="string" :validations="props.validations" v-model:input-class="inputClass">
+    <input
+      type="text"
+      :class="inputClass"
+      :placeholder="props.placeholder"
+      v-model="string"
+    />
+  </VInputField>
+</template>
+
+<style scoped>
+</style>
