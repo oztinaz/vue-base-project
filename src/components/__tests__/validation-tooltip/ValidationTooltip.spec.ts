4227e29e14c320079c50e075037fccbe551eb626
Oğuz Öztınaz <oguzoztinaz0@gmail.com>
2024-02-29 15:47:04 +0300

    Implement ValidationTooltip component

---
+++ src/components/__tests__/validation-tooltip/ValidationTooltip.spec.ts
@@ -0,0 +1,25 @@
+import ValidationTooltip from '@/components/validation-tooltip/ValidationTooltip.vue'
+import { mount, type VueWrapper } from '@vue/test-utils'
+import { beforeEach, describe, expect, test } from 'vitest'
+import { faker } from '@faker-js/faker'
+import { ArrayUtils } from '@/utils/Array'
+import type { Validation } from '@/types/Validation'
+
+describe('@/components/validation-tooltip/ValidationTooltip', () => {
+  let wrapper: VueWrapper
+  const input: string = faker.lorem.word()
+  const validations: Validation[] = []
+
+  beforeEach(() => {
+    wrapper = mount(ValidationTooltip, {
+      props: {
+        input: input,
+        validations: validations
+      }
+    })
+  })
+
+  test('checks default position', () => {
+    expect(wrapper.vm.position).toBe('left')
+  })
+})
---
+++ src/components/validation-tooltip/ValidationTooltipContent.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import ValidationTooltipContentItem from './ValidationTooltipContentItem.vue'
+import type { Validation as TValidation } from '@/types/Validation'
+
+const props = defineProps<{
+  input: any
+  validations: TValidation[]
+}>()
+</script>
+
+<template>
+  <div class="validation-tooltip-content">
+    <ValidationTooltipContentItem
+      v-for="validation in props.validations"
+      :key="validation.explanation"
+      :input="props.input"
+      :validation="validation"
+      data-test="validation"
+    />
+  </div>
+</template>
+
+<style scoped>
+.validation-tooltip-content {
+  display: flex;
+  flex-direction: column;
+  gap: 5px;
+}
+</style>
