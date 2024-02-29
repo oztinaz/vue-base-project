ed2598882759677c92df0f8ef5ce4d4264372c9b
Oğuz Öztınaz <oguzoztinaz0@gmail.com>
2024-02-29 20:58:45 +0300

    Implement TimeField component

---
+++ src/components/fields/input-fields/datetime-input-fields/TimeField.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import VInputField from '../VInputField.vue'
+import VTimeField from './VTimeField.vue'
+import { ref, type Ref } from 'vue';
+import type { Validation } from '@/types/Validation'
+
+const time = defineModel('time', {
+  required: true,
+  type: String
+})
+
+const props = defineProps<{
+  placeholder?: string
+  validations?: Validation[]
+}>()
+
+const inputClass: Ref<string> = ref('')
+</script>
+
+<template>
+  <VInputField :input="time" :validations="props.validations" v-model:input-class="inputClass">
+    <VTimeField :input-class="inputClass" :placeholder="props.placeholder" v-model:time="time" />
+  </VInputField>
+</template>
+
+<style scoped></style>
