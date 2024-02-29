072927af0624fb7ca2de5cefd28913d6a2899620
Oğuz Öztınaz <oguzoztinaz0@gmail.com>
2024-02-29 20:52:34 +0300

    Implement VTimeField component

---
+++ src/components/fields/input-fields/datetime-input-fields/VTimeField.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+const time = defineModel('time', {
+  required: true,
+  type: String
+})
+
+const props = defineProps<{
+  inputClass: string
+  placeholder?: string
+}>()
+</script>
+
+<template>
+  <input
+    type="time"
+    :class="inputClass"
+    :placeholder="props.placeholder"
+    v-model="time"
+  />
+</template>
+
+<style scoped></style>
