d3a4dc555f7b437067651a96d88d838e3dde16ec
Oğuz Öztınaz <oguzoztinaz0@gmail.com>
2024-03-01 16:40:38 +0300

    Implement ImageField component

---
+++ src/components/fields/image-fields/ImageField.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+const props = defineProps<{
+  height: number
+  src: string
+  width: number
+}>()
+</script>
+
+<template>
+  <img :src="props.src" alt="Image" :height="props.height" :width="props.width" />
+</template>
+
+<style scoped></style>
