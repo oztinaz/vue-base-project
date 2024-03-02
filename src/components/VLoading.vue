1196161f62fc66bdbcf9694352021179ee552970
Oğuz Öztınaz <oguzoztinaz0@gmail.com>
2024-03-02 11:38:06 +0300

    Implement VLoading component

---
+++ src/components/VLoading.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import ImageField from '@/components/fields/image-fields/ImageField.vue'
+import { ref, type Ref } from 'vue'
+
+const props = defineProps<{
+  height: number
+  width: number
+}>()
+
+const loadingGifPath: Ref<string> = ref('/src/assets/loading.gif')
+</script>
+
+<template>
+  <ImageField :src="loadingGifPath" :height="props.height" :width="props.width" />
+</template>
